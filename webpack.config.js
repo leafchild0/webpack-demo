20940f5c553ac02a15f1c4199646f2aabb750d02
Victor Malyshev <vmalyshev0@gmail.com>
2019-07-16 15:31:35 +0300

    added html autogeneration plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     index: ['./src/index.js', './src/component.js']
     //component: './src/component.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Testing webpack'
+    })
+  ],
   mode: 'production',
   output: {
     filename: '[name].js',
