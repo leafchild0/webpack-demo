6b55f704cea5106018e9936650b05220ff1fbf02
Victor Malyshev <vmalyshev0@gmail.com>
2019-07-16 16:25:14 +0300

    added babel and es6 transpilation

--- webpack.common.js
+++ webpack.common.js
@@ -25,6 +25,17 @@ module.exports = {
         "css-loader", // translates CSS into CommonJS
         "sass-loader" // compiles Sass to CSS, using Node Sass by default
       ]
-    }]
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
   }
 };
