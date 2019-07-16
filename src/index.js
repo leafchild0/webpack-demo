2da223f4c24079a72928b33b5806e72b9e9b8fac
Victor Malyshev <vmalyshev0@gmail.com>
2019-07-16 15:39:42 +0300

    added SASS compillation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import join from 'lodash/join';
 import comp from './component';
+import '../assets/styles.scss';
 
 function component() {
 	let element = document.createElement('div');
   
 	element.innerHTML = join(['Hello', 'webpack'], ' ');
+	element.classList.add('test');
+
 	console.log('component input');
 	comp.test();
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     index: ['./src/index.js', './src/component.js']
     //component: './src/component.js'
+  },
+  mode: 'production',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -13,9 +20,14 @@ module.exports = {
       title: 'Testing webpack'
     })
   ],
-  mode: 'production',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+      ]
+    }]
   }
 };
